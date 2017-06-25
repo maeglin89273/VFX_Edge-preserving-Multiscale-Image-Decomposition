fb5f388903edeee2f182a746830a49e642a3060c
maeglin89273 <maeglin89273@gmail.com>
2017-06-26 00:46:46 +0800

    #Add demo code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2, utils
+import numpy as np
 import decompose
 import sys
 
@@ -11,9 +12,23 @@ if __name__ == '__main__':
     imgYCC = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
     imgY = imgYCC[:, :, 0]
     M, Ds = decompose.edge_preserving_decompose(imgY, int(sys.argv[2]))
-    # M = decompose.adjust_constrast(imgY, M)
-    imgYCC[:, :, 0] = M
-    imgBGR = cv2.cvtColor(imgYCC, cv2.COLOR_YCR_CB2BGR)
-    utils.show_image(imgBGR)
-    cv2.imwrite('de_' + filename, imgBGR)
+    enhanced_factor = 2
+
+    adj_cont_M = decompose.adjust_constrast(imgY, M)
+    m_show = np.copy(imgYCC)
+    detail_enhanced = np.copy(imgYCC)
+
+    m_show[:, :, 0] = adj_cont_M
+    detail_enhanced[:, :, 0] = decompose.clip_and_convert_to_uint8(M + enhanced_factor * np.sum(Ds, axis=0))
+    # detail_enhanced[:, :, 0] = decompose.clip_and_convert_to_uint8(M + enhanced_factor * Ds[0] + Ds[1])
+    # detail_enhanced[:, :, 0] = decompose.clip_and_convert_to_uint8(M + Ds[0] + enhanced_factor * Ds[1])
+
+    m_show = cv2.cvtColor(m_show, cv2.COLOR_YCR_CB2BGR)
+    detail_enhanced = cv2.cvtColor(detail_enhanced, cv2.COLOR_YCR_CB2BGR)
+
+    utils.show_images([img, m_show], ['original', 'M'])
+    utils.show_images([img, detail_enhanced], ['original', 'detail enhanced'])
+
+    cv2.imwrite('m_' + filename, m_show)
+    cv2.imwrite('de_' + filename, detail_enhanced)
 
